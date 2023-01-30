939c5d0f61e6d699b03700d09346ec851a67174f
jimmycasta <jimmycasta@gmail.com>
2023-01-30 08:55:33 -0500

    Teminacion del Proyecto

---
+++ src/main/java/com/example/webfiltrar/WebFiltrarApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfiltrar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFiltrarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFiltrarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webfiltrar/WebFiltrarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfiltrar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFiltrarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
